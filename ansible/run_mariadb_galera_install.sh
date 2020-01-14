1d0383e59f6a0240fcb5ee677833061ca22673aa
Emerson Gaudencio <emerson.fender@gmail.com>
2020-01-14 02:20:51 +0000

    Release Candidate 2.0.0

--- ansible/run_mariadb_galera_install.sh
+++ ansible/run_mariadb_galera_install.sh
@@ -13,6 +13,36 @@ VAR_PRIMARY_SERVER="$4"
 VAR_GALERA_CLUSTER_NAME="$5"
 VAR_GALERA_CLUSTER_ADDRESS="$6"
 
+if [ "${VAR_HOST}" == '' ] ; then
+  echo "No host specified. Please have a look at README file for futher information!"
+  exit 1
+fi
+
+if [ "${VAR_MARIADB_VERSION}" == '' ] ; then
+  echo "No MariaDB version specified. Please have a look at README file for futher information!"
+  exit 1
+fi
+
+if [ "${VAR_GTID}" == '' ] ; then
+  echo "No GTID domain specified. Please have a look at README file for futher information!"
+  exit 1
+fi
+
+if [ "${VAR_PRIMARY_SERVER}" == '' ] ; then
+  echo "No Primary Server specified. Please have a look at README file for futher information!"
+  exit 1
+fi
+
+if [ "${VAR_GALERA_CLUSTER_NAME}" == '' ] ; then
+  echo "No Galera Cluster Name specified. Please have a look at README file for futher information!"
+  exit 1
+fi
+
+if [ "${VAR_GALERA_CLUSTER_ADDRESS}" == '' ] ; then
+  echo "No Galera Cluster Address specified. Please have a look at README file for futher information!"
+  exit 1
+fi
+
 ### Ping host ####
 ansible -i $SCRIPT_PATH/hosts -m ping $VAR_HOST -v
 
